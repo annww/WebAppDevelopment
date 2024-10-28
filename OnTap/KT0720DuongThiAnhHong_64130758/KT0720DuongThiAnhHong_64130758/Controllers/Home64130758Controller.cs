00f834e67542a2f3be29e7fd92376f0bcca6bb35
Anh Hong <samh5813p@gmail.com>
2024-10-29 00:06:47 +0700

    no message

---
+++ OnTap/KT0720DuongThiAnhHong_64130758/KT0720DuongThiAnhHong_64130758/Controllers/Home64130758Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KT0720DuongThiAnhHong_64130758.Controllers
+{
+    public class Home64130758Controller : Controller
+    {
+        // GET: Home64130758
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ OnTap/KT0720DuongThiAnhHong_64130758/KT0720DuongThiAnhHong_64130758/Controllers/SinhVien_64130758Controller.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using KT0720DuongThiAnhHong_64130758.Models;
+
+namespace KT0720DuongThiAnhHong_64130758.Controllers
+{
+    public class SinhVien_64130758Controller : Controller
+    {
+        private Model_64130758Entities db = new Model_64130758Entities();
+        string LayMaSV()
+        {
+            var maMax = db.SinhViens.ToList().Select(n => n.MaSV).Max();
+            int maNV = int.Parse(maMax.Substring(2)) + 1;
+            string NV = String.Concat("000", maNV.ToString());
+            return "NV" + NV.Substring(maNV.ToString().Length - 1);
+        }
+        // GET: SinhVien_64130758
+        public ActionResult Index()
+        {
+            var sinhViens = db.SinhViens.Include(s => s.Lop);
+            return View(sinhViens.ToList());
+        }
+
+        // GET: SinhVien_64130758/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SinhVien sinhVien = db.SinhViens.Find(id);
+            if (sinhVien == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sinhVien);
+        }
+
+        // GET: SinhVien_64130758/Create
+        public ActionResult Create()
+        {
+            ViewBag.MaSV = LayMaSV();
+            ViewBag.MaLop = new SelectList(db.Lops, "MaLop", "TenLop");
+            return View();
+        }
+
+        // POST: SinhVien_64130758/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MaSV,HoSV,TenSV,NgaySinh,GioiTinh,AnhSV,DiaChi,MaLop")] SinhVien sinhVien)
+        {
+            var imgSV = Request.Files["Avatar"];
+            //Lấy thông tin từ input type=file có tên Avatar
+            string postedFileName = System.IO.Path.GetFileName(imgSV.FileName);
+            //Lưu hình đại diện về Server
+            var path = Server.MapPath("/images/" + postedFileName);
+            imgSV.SaveAs(path);
+
+            if (ModelState.IsValid)
+            {
+                sinhVien.MaSV = LayMaSV();
+                sinhVien.AnhSV = postedFileName;
+                db.SinhViens.Add(sinhVien);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.MaLop = new SelectList(db.Lops, "MaLop", "TenLop", sinhVien.MaLop);
+            return View(sinhVien);
+        }
+
+        // GET: SinhVien_64130758/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SinhVien sinhVien = db.SinhViens.Find(id);
+            if (sinhVien == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.MaLop = new SelectList(db.Lops, "MaLop", "TenLop", sinhVien.MaLop);
+            return View(sinhVien);
+        }
+
+        // POST: SinhVien_64130758/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MaSV,HoSV,TenSV,NgaySinh,GioiTinh,AnhSV,DiaChi,MaLop")] SinhVien sinhVien)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(sinhVien).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.MaLop = new SelectList(db.Lops, "MaLop", "TenLop", sinhVien.MaLop);
+            return View(sinhVien);
+        }
+
+        // GET: SinhVien_64130758/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SinhVien sinhVien = db.SinhViens.Find(id);
+            if (sinhVien == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sinhVien);
+        }
+
+        // POST: SinhVien_64130758/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            SinhVien sinhVien = db.SinhViens.Find(id);
+            db.SinhViens.Remove(sinhVien);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        public ActionResult GioiThieu_64130758(string id)
+        {
+            return View();
+        }
+
+        public ActionResult TimKiem_64130758()
+        {
+            var nhanViens = db.SinhViens.Include(n => n.Lop);
+            return View(nhanViens.ToList());
+        }
+        [HttpPost]
+        public ActionResult TimKiem_64130758(string maSV)
+        {
+            var nhanViens = db.SinhViens.Where(a => a.MaSV.Contains(maSV));
+            return View(nhanViens.ToList());
+        }
+    }
+}
